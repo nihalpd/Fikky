cd3e87d409269b98a5038b5550070114d80800c4
Nihal Dayal <nihaldayal@gmail.com>
2016-10-13 19:27:03 -0700

    Enable SSO

--- src/main/java/com/fikky/FikkyApplication.java
+++ src/main/java/com/fikky/FikkyApplication.java
@@ -2,8 +2,11 @@ package com.fikky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class FikkyApplication {
 
 	public static void main(String[] args) {
